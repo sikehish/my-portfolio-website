9d3dea0770b7ed9a19bb0e1796ffa256d975b7c2
Syed Hisham Akmal <hisham0502@gmail.com>
2023-06-19 18:45:05 +0530

    Minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en">   
       <head />
       <body>
         <ThemeProvider enableSystem={true} attribute="class">
